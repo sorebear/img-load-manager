9787c7e08e88c2b801068f4959201da6e8fe7882
Soren Baird <sorenbaird@gmail.com>
2017-12-08 10:15:09 -0800

    removed all es6 code for compatability

--- img-load-manager/img_script.js
+++ img-load-manager/img_script.js
@@ -1,54 +1,37 @@
 /**
- * imageManagerClass handles methods related to swapping low-res photos
+ * imageManagerConstructor handles methods related to swapping low-res photos
  * to high-res on load completion.
  */
-class imageManagerClass {
+function ImageManagerConstructor() {
    /**
     * On instantiation, the constructor will find and store all photos in DOM
-    * Then run the init() method
     */
-   constructor() {
-      /**
-       * Creates a node list of all the high resolution photos in the DOM
-       */
-      this.highResArray =
-         document.getElementsByClassName('img-load-mgr__high-res');
-      this.lowResArray =
-         document.getElementsByClassName('img-load-mgr__low-res');
-      this.init();
-   }
-   /**
-    * The init method is called on instantiation
-    * And delegates initializing functions.
-    */
-   init() {
-      this.swapAllImages();
-   }
+   var highResArray = document.getElementsByClassName('img-load-mgr__high-res');
+   var lowResArray = document.getElementsByClassName('img-load-mgr__low-res');
 
    /**
    * Loops through high-res photos, instructing them to become opaque on load
    * Instructs high-res/low-res images to shrink after the high-res photo loads
    */
-   swapAllImages() {
-      const self = this;
-      for (let i = 0; i <this.highResArray.length; i++) {
+   swapAllImages = function() {
+      for (var i = 0; i < highResArray.length; i++) {
          (function(i) {
             setTimeout(function() {
-               if (self.highResArray[i].complete) {
-                  self.highResArray[i].classList.remove(
+               if (highResArray[i].complete) {
+                  highResArray[i].classList.remove(
                      'img-load-mgr__hidden',
                      'img-load-mgr__enlarge'
                   );
-                  self.lowResArray[i].classList.remove(
+                  lowResArray[i].classList.remove(
                      'img-load-mgr__enlarge'
                   );
                } else {
-                  self.highResArray[i].onload = function() {
-                     self.highResArray[i].classList.remove(
+                  highResArray[i].onload = function() {
+                     highResArray[i].classList.remove(
                         'img-load-mgr__hidden',
                         'img-load-mgr__enlarge'
                      );
-                     self.lowResArray[i].classList.remove(
+                     lowResArray[i].classList.remove(
                         'img-load-mgr__enlarge'
                      );
                   };
@@ -56,7 +39,9 @@ class imageManagerClass {
             });
          })(i);
       }
-   }
+   };
+
+   swapAllImages();
 }
 
-const imageManager = new imageManagerClass;
+var imageManager = new ImageManagerConstructor;
